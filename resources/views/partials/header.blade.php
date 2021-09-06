bc16eac623785417fd7b4c57075b221b34114484
maicol <maicol.matteoli@gmail.com>
2021-09-06 18:04:11 +0200

    add : header top

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,24 @@
 <header>
     <!-- <h1>sono un header!</h1> -->
+    <div class="header_top">
+      testo di prova
+    </div>
     <div class="wrap">
       <div class="logo">
         <img src="{{ asset('images/dc-logo.png') }}" alt="header logo">
       </div>
       <ul>
-        <li>item menu</li>
-        <li>item menu</li>
-        <li>item menu</li>
+        <li><a>characters</a></li>
+        <li><a>comics</a></li>
+        <li><a>movies</a></li>
+        <li><a>tv</a></li>
+        <li><a>games</a></li>
+        <li><a>collectibles</a></li>
+        <li><a>videos</a></li>
+        <li><a>fans</a></li>
+        <li><a>news</a></li>
+        <li><a>shop</a></li>
+        <li><input type="text" placeholder="search"> </li>
 
       </ul>
     </div>
