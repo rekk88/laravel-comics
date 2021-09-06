48f9ddd8a067ccacbb8d844430182ebff856c3d8
maicol <maicol.matteoli@gmail.com>
2021-09-06 17:50:15 +0200

    add : images + header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    <h1>sono un header!</h1>
+    <!-- <h1>sono un header!</h1> -->
+    <div class="wrap">
+      <div class="logo">
+        <img src="{{ asset('images/dc-logo.png') }}" alt="header logo">
+      </div>
+      <ul>
+        <li>item menu</li>
+        <li>item menu</li>
+        <li>item menu</li>
+
+      </ul>
+    </div>
 </header>
